2dd18466e45c918470003b11fec44a410f28c2b5
Andy Pan <andy0130tw@yahoo.com.tw>
2015-10-13 03:45:25 +0800

    fix hw2 #define in A and B

--- hw2/HW2A002.cpp
+++ hw2/HW2A002.cpp
@@ -1,16 +1,18 @@
 ﻿//================================================================
-//  PROGRAMMER  : 潘廣霖 
+//  PROGRAMMER  : 潘廣霖
 //  DATE        : 2015-10-08
 //  FILENAME    : HW02A002.CPP
 //  DESCRIPTION : This is a program to print information about three integers
 //================================================================
-#define max3(a, b, c) max(max(a, b), c)
-#define min3(a, b, c) min(min(a, b), c)
 
 #include "stdafx.h"
 #include<iostream>
 #include<algorithm>
 #include<cstdlib>
+// make use of the max/min function in std::algorithm
+// ref: http://stackoverflow.com/q/19897722/2281355
+#define max3(a, b, c) max(max(a, b), c)
+#define min3(a, b, c) min(min(a, b), c)
 using namespace std;
 
 int main(int argc, _TCHAR* argv[]) {
@@ -23,7 +25,7 @@ int main(int argc, _TCHAR* argv[]) {
   cout << "Product is "         << (a * b * c)   << endl;
   cout << "Smallest number is " << min3(a, b, c) << endl;
   cout << "Largest number is "  << max3(a, b, c) << endl;
-  
+
   system("pause");
   return 0;
 }
--- hw2/HW2D002.cpp
+++ hw2/HW2D002.cpp
@@ -1,17 +1,16 @@
 ﻿//================================================================
-//  PROGRAMMER  : 潘廣霖 
+//  PROGRAMMER  : 潘廣霖
 //  DATE        : 2015-10-08
 //  FILENAME    : HW02D002.CPP
 //  DESCRIPTION : This is a program to help calculate the sum of 3 cards in a blackjack game
 //================================================================
 
-// not to rely on the max function in std::algorithm
-#define min(a, b) ((a) < (b) ? (a) : (b))
-
 #include "stdafx.h"
 #include<iostream>
 #include<cstdlib>
 #include<cmath>
+// not to rely on the max function in std::algorithm
+#define min(a, b) ((a) < (b) ? (a) : (b))
 using namespace std;
 
 int main(int argc, _TCHAR* argv[]) {
@@ -28,14 +27,14 @@ int main(int argc, _TCHAR* argv[]) {
   // if (a > 10) sum += 10; else sum += a;
   // ... for b and c
   sum += min(a, 10) + min(b, 10) + min(c, 10);
- 
+
   // promote ace to 11 points if the result does not exceed 21.
   // there is no need to handle multiple aces(?),
   // because it will not satisfy the condition.
   if (sum <= 11 && ace) sum += 10;
 
   cout << "These cards yield " << sum << " point(s)." << endl;
-  
+
   system("pause");
   return 0;
 }
